789194c0f35efaedc2e7d7a44d464360c8e735ae
Teo-Medesi <118106954+Teo-Medesi@users.noreply.github.com>
2022-12-18 17:41:46 +0000

    added image upload for header

--- src/components/UploadPopUp.js
+++ src/components/UploadPopUp.js
@@ -1,39 +1,65 @@
 import React, {useState, useEffect} from 'react'
 import BirdImage from "./images/bird.png"
+import CloseIcon from "./images/close.png"
 
-const UploadPopUp = ({uploadFunction, hidden}) => {
+// ONLY ONE INSTANCE OF UPLOAD MODAL CAN EXIST UPON RENDER
+const UploadPopUp = ({uploadFunction, isHidden, setIsActive}) => {
 
-    const [file, setFile] = useState(0);
-    const [previewFileURL, setPreviewFileURL] = useState("");
+    const initialState = {
+        file: 0,
+        previewFileURL: "",
+        isUploaded: false
+    }
+
+    const [file, setFile] = useState(initialState.file);
+    const [previewFileURL, setPreviewFileURL] = useState(initialState.previewFileURL)
+    const [isUploaded, setIsUploaded] = useState(initialState.isUploaded);
 
     const handleClick = () => {
         document.getElementById("input").click();
     }
 
     useEffect(() => {
-        if (file !== 0)
+        if (file !== 0 && file !== null)
         {
             console.log("File changed")
             setPreviewFileURL(URL.createObjectURL(file));
+            console.log(previewFileURL);
+            console.log(file.name);
         }
     },[file]);
 
+
+    // TO-DO make this shit work, state resets but causes errors when trying to upload images
+     useEffect(() => {
+        setIsUploaded(initialState.isUploaded);
+    //    setFile(initialState.file);
+    //    setPreviewFileURL(initialState.previewFileURL);
+    }, [isHidden]);
+ 
     const handleUploadClick = () => {
-        uploadFunction(file);
+        uploadFunction(file, setIsUploaded);
+    }
+
+    const handleClose = () => {
+        setIsActive(false);
     }
 
     return (
-        <div className={hidden ? 'w-screen h-screen fixed py-12 justify-center bg-[rgb(0,0,0,0.1)] hidden' : 'w-screen h-screen fixed py-12 justify-center flex bg-[rgb(0,0,0,0.1)]'}>
-            <div className='flex flex-wrap justify-between bg-gray-100 w-[75%] xl:w-[50%] rounded-xl p-6 h-full'>
-                <input id="input" onChange={event => setFile(event.target.files[0])} type="file" className="hidden"/>
-                <button onClick={handleClick} className='basis-[28%] h-min bg-gradient-to-r from-gray-800 to-indigo-900 text-xl text-white rounded-md p-4'>Select Image</button>
-                <div className='text-center italic text-gray-600 bg-white border rounded-md border-gray-200 text-xl p-4 h-min w-full basis-[70%]'>{(file.name == null) ? "Click on the select image button" : file.name}</div>
+        <div className={isHidden ? 'w-screen h-screen fixed justify-center bg-[rgb(0,0,0,0.2)] hidden' : 'w-screen h-screen fixed top-0 left-0 right-0 bottom-0 py-12 justify-center flex bg-[rgb(0,0,0,0.1)]'}>
+            <div className='flex flex-col gap-24 bg-gray-100 w-[75%] xl:w-[50%] rounded-xl p-6 h-full'>
+                <div className='flex justify-between'>
+                    <input id="input" onChange={event => setFile(event.target.files[0])} type="file" className="hidden"/>
+                    <button onClick={handleClick} className='basis-[28%] h-min bg-gradient-to-r from-gray-800 to-indigo-900 text-xl text-white rounded-md p-4'>Select Image</button>
+                    <div className='text-center italic text-gray-600 bg-white border rounded-md border-gray-200 text-xl p-4 h-min w-full basis-[60%]'>{(file.name == null) ? "Click on the select image button" : file.name}</div>
+                    <img src={CloseIcon} onClick={handleClose} className="w-[66px] h-[66px] basis-[5%] cursor-pointer hover:bg-gray-300 p-1 rounded-md"/>
+                </div>
 
-                <div className='flex flex-col gap-12 justify-center items-center mt-5 border-gray-500 border rounded-xl w-full h-[460px] py-5'>
+                <div className='flex flex-col gap-12 justify-center items-center mt-5 border-gray-500 border rounded-xl w-full h-full py-5'>
                     <div className={(file == 0) ? 'text-2xl text-gray-600 italic' : 'text-2xl text-gray-600 italic hidden'}>Your image will be displayed here!</div>
-                    <img src={(file == 0) ? BirdImage : previewFileURL} className={(file == 0) ? "w-60 h-60" : "max-w-[250px] max-h-[250px]"}/>
+                    <img src={(file == 0) ? BirdImage : previewFileURL} className={(file == 0) ? "w-56 h-56 " : "max-w-[250px] max-h-[250px]"}/>
 
-                    <button onClick={handleUploadClick} className='h-min w-96 bg-gray-800 text-xl text-white rounded-md p-4'>Upload</button>
+                    <button onClick={handleUploadClick} className={isUploaded ? 'h-min w-96 bg-gradient-to-r from-[#2d388a] to-[#00aeef] text-xl text-white rounded-md p-4' : 'h-min w-96 bg-gray-800 text-xl text-white rounded-md p-4'}>{isUploaded ? "Image uploaded!" : "Upload"}</button>
                 </div>
 
             </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,116 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from "./images/addIcon.png"
+import { storage } from '../../firebase.config'
+import { getDownloadURL, listAll, ref, uploadBytes, deleteObject } from "firebase/storage"
+import UploadPopUp from '../UploadPopUp'
 
 
 const Header = () => {
-  return (
-    <div className='h-[800px] bg-gray-100 py-8 px-16 transition ease-in-out duration-300 hover:brightness-50 cursor-pointer'>
-        <div className='flex flex-col justify-center items-center border-2 rounded border-gray-300 w-full h-full'>
-            <img src={Logo} className="w-36 h-36"/>
-            <p className='text-2xl uppercase text-gray-500 tracking-wide mt-4'>Click to upload image</p>
-        </div>
 
-    </div>
-  )
+  const InitialState = {
+    imageURL: "",
+    isUploadModalActive: false
+  }
+
+  // first off, we need to fetch the image from our storage, we need an imageURL state
+  const [imageURL, setImageURL] = useState(InitialState.imageURL);
+  const [isUploadModalActive, setIsUploadModalActive] = useState(InitialState.isUploadModalActive);
+
+  const fetchImage = () => {
+    const imagesRef = ref(storage, "images/headerImage" );
+
+    listAll(imagesRef).then(list => {
+      list.items.map(itemRef => getDownloadURL(itemRef).then(url => {
+        setImageURL(url);
+      }));
+    })
+    console.log(imageURL)
+  }
+
+  useEffect(() => {
+    fetchImage();
+  },[]);
+  
+  const uploadImage = async (file, setIsUploaded) => {
+    if (file == 0){
+      console.log("no file found");
+      return;
+    }
+    
+    // here we reference the path to which we want to store our image, not the folder in which we want it to be
+    const imageRef = ref(storage, "images/headerImage/" + file.name);        
+    const imagesRef = ref(storage, "images/headerImage")
+
+    listAll(imagesRef).then(list => {
+      list.items.map(itemRef => deleteObject(itemRef))
+    });
+
+
+    uploadBytes(imageRef, file).then(() => {
+      fetchImage();
+
+      if (setIsUploaded !== null)
+      {
+        setIsUploaded(true);
+      }
+    }).catch(error => console.log("error occured upon upload"));
+
+  }
+
+  const handleUploadClick = () => {
+    setIsUploadModalActive(true);
+  }
+
+  if (isUploadModalActive === true)
+  {
+    if (imageURL === InitialState.imageURL) 
+    {
+      return (
+        <div>
+          <UploadPopUp uploadFunction={uploadImage} setIsActive={setIsUploadModalActive} isHidden={isUploadModalActive ? false : true}/>
+          <div onClick={handleUploadClick} className={isUploadModalActive ? 'h-[800px] bg-gray-100 py-8 px-16' : 'h-[800px] bg-gray-100 py-8 px-16 transition ease-in-out duration-300 hover:brightness-50 cursor-pointer'}>
+              <div className='flex flex-col justify-center items-center border-2 rounded border-gray-300 w-full h-full'>
+                  <img src={Logo} className="w-36 h-36"/>
+                  <p className='text-2xl uppercase text-gray-500 tracking-wide mt-4'>Click to upload image</p>
+              </div>
+    
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <UploadPopUp uploadFunction={uploadImage} setIsActive={setIsUploadModalActive} isHidden={isUploadModalActive ? false : true}/>  
+          <div onClick={handleUploadClick} className={isUploadModalActive ? "bg-no-repeat bg-cover h-[800px] max-h-[800px] w-full" : "bg-no-repeat transition ease-in-out duration-300 hover:brightness-50 cursor-pointer bg-cover h-[800px] max-h-[800px] w-full"} style={{backgroundImage: "url(" + imageURL + ")"}}>
+          </div>
+        </div>
+      )
+    }
+  }
+  else {
+    if (imageURL === InitialState.imageURL) 
+    {
+      return (
+        <div>
+          <div onClick={handleUploadClick} className={isUploadModalActive ? 'h-[800px] bg-gray-100 py-8 px-16' : 'h-[800px] bg-gray-100 py-8 px-16 transition ease-in-out duration-300 hover:brightness-50 cursor-pointer'}>
+              <div className='flex flex-col justify-center items-center border-2 rounded border-gray-300 w-full h-full'>
+                  <img src={Logo} className="w-36 h-36"/>
+                  <p className='text-2xl uppercase text-gray-500 tracking-wide mt-4'>Click to upload image</p>
+              </div>
+    
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div onClick={handleUploadClick} className="bg-no-repeat transition ease-in-out duration-300 hover:brightness-50 cursor-pointer bg-cover h-[800px] max-h-[800px] w-full" style={{backgroundImage: "url(" + imageURL + ")"}}>
+          
+        </div>
+      )
+    }
+  }
 }
 
 
