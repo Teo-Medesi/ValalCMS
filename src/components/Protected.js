f981b6f6368f71d20ebc0c5dbd8ee0c076dc5a2c
Teo-Medesi <118106954+Teo-Medesi@users.noreply.github.com>
2023-01-02 13:19:57 +0000

    added basic account functionality

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth } from '../firebase.config'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { onAuthStateChanged } from 'firebase/auth'
-import { Children } from 'react'
+import { UserContext } from '../App'
+import { useEffect } from 'react'
 
-const Protected = () => {
+const Protected = ({children}) => {
     const navigate = useNavigate();
-    
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            return Children;
-        }
-        else {
-            navigate("/sign-up");
-        }   
-    })
+    const user = useContext(UserContext);
 
+    useEffect(() => {
+        if (user !== [])
+        {
+            if(!user) navigate("/");
+        }
+    }, [user]);
+
+    if(user) return children;
 }
 
 export default Protected
